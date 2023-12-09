6844901cc0e601909f683c3a48721763672ebdc6
Francesco Azzurri <101581528+FrancescoAzzurri@users.noreply.github.com>
2023-12-09 09:44:42 +0100

    Add files via upload

---
+++ C02/ex00/ft_strcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fazzurri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 17:35:35 by fazzurri          #+#    #+#             */
+/*   Updated: 2023/12/07 16:01:39 by fazzurri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcpy(char *dest, const char *src)
+
+{
+	int	count;
+
+	count = 0;
+	while (src[count] != '\0')
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	dest[count] = '\0';
+	return (dest);
+}

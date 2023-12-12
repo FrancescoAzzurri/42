b107bcdb52bce048021151842ece77aee36f2e3b
Francesco Azzurri <101581528+FrancescoAzzurri@users.noreply.github.com>
2023-12-12 16:01:37 +0100

    Add files via upload

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fazzurri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/12 14:56:54 by fazzurri          #+#    #+#             */
+/*   Updated: 2023/12/12 15:32:38 by fazzurri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	char	*inizialization;	
+
+	inizialization = dest;
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	return (inizialization);
+}
